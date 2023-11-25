d2c9a7d96f3a4b9ecd04501cc0fe5d90bfe2c4dd
thuaba22 <mim.islam.tabassuma@gmail.com>
2023-11-25 22:43:53 +0600

    get operation done for meals data

--- index.js
+++ index.js
@@ -25,15 +25,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
+    const mealsCollection = client.db("mealsDB").collection("meals");
+    app.get("/meals", async (req, res) => {
+      const cursor = mealsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
